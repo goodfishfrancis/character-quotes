3bd66dcc1decb45c33025f4815573f0d94c04703
Alexander Solinger <alexandersolinger@me.com>
2023-10-14 13:29:23 -0700

    initial commit

---
+++ src/main/java/com/odenfish/characterquotes/CharacterQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.odenfish.characterquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odenfish/characterquotes/CharacterQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odenfish.characterquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
