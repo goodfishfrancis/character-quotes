f972f56d482e042979f760a65b342afcfec6ce74
Alexander Solinger <alexandersolinger@me.com>
2023-10-15 13:44:17 -0700

    UPDATE: adds generated value to primary keys

--- src/main/java/com/odenfish/characterquotes/models/Persona.java
+++ src/main/java/com/odenfish/characterquotes/models/Persona.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -19,6 +21,7 @@ import lombok.AllArgsConstructor;
 public class Persona {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
--- src/main/java/com/odenfish/characterquotes/models/Quote.java
+++ src/main/java/com/odenfish/characterquotes/models/Quote.java
@@ -3,6 +3,8 @@ package com.odenfish.characterquotes.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,6 +21,7 @@ import lombok.Setter;
 public class Quote {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
 	
