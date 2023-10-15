7db9914d5cab970f01078560fc404842335da764
Alexander Solinger <alexandersolinger@me.com>
2023-10-15 13:39:26 -0700

    UPDATE: adds PersonaService

---
+++ src/main/java/com/odenfish/characterquotes/services/PersonaService.java
@@ -0,0 +1,24 @@
+package com.odenfish.characterquotes.services;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.odenfish.characterquotes.models.Persona;
+import com.odenfish.characterquotes.repositories.PersonaRepository;
+import com.odenfish.characterquotes.repositories.QuoteRepository;
+
+@Service
+public class PersonaService {
+	
+	@Autowired
+	PersonaRepository personaRepository;
+	
+	@Autowired
+	QuoteRepository quoteRepository;
+	
+	public List<Persona> list() {
+        return personaRepository.findAll();
+    }
+
+}
