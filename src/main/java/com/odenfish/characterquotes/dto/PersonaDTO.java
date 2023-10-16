a871ebf0ff8884b286dc98c351b3a7bf8e3f3678
Alexander Solinger <alexandersolinger@me.com>
2023-10-15 20:35:16 -0700

    UPDATE: adds Dto classes

---
+++ src/main/java/com/odenfish/characterquotes/dto/PersonaDTO.java
@@ -0,0 +1,28 @@
+package com.odenfish.characterquotes.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonaDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private String name;
+	
+	private List<QuoteDTO> quotes;
+
+}
---
+++ src/main/java/com/odenfish/characterquotes/dto/QuoteDTO.java
@@ -0,0 +1,25 @@
+package com.odenfish.characterquotes.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuoteDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private String quote;
+
+}
