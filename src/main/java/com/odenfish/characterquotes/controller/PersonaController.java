8d51b3cb246387ed9ff4710e000b568a7f4c6cfd
Alexander Solinger <alexandersolinger@me.com>
2023-10-16 12:42:22 -0700

    UPDATE: adds request method for getting persona by id

--- src/main/java/com/odenfish/characterquotes/controller/PersonaController.java
+++ src/main/java/com/odenfish/characterquotes/controller/PersonaController.java
@@ -1,12 +1,14 @@
 package com.odenfish.characterquotes.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +43,36 @@ public class PersonaController {
 		
 		return response;
 		
+	}
+	
+	@GetMapping("/characters/{id}")
+	public ResponseEntity<PersonaDTO> getPersonaById(@PathVariable("id") long id) {
+		
+		
+		ResponseEntity<PersonaDTO> response = null;
+		
+		try {
+			
+			PersonaDTO personaDTO = personaService.getById(id);
+			
+			if (personaDTO != null) {
+				
+				response = new ResponseEntity<>(personaDTO, HttpStatus.OK);
+				
+			}
+			else {
+				
+				response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+				
+			}
+		}
+		catch (Exception e) {
+			
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		
+		return response;
 	}
 	
 }
