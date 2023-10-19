5e460f2975e191dec5334cb4685cb2bdc1e6c0d6
Alexander Solinger <alexandersolinger@me.com>
2023-10-19 13:32:49 -0700

    UPDATE: adds cross origin for localhost:3000

--- src/main/java/com/odenfish/characterquotes/controller/PersonaController.java
+++ src/main/java/com/odenfish/characterquotes/controller/PersonaController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.odenfish.characterquotes.dto.PersonaDTO;
 import com.odenfish.characterquotes.services.PersonaService;
 
-//@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/persona")
 public class PersonaController {
